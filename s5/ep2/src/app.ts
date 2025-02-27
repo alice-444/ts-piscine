5a2814c818ec49d29dab356db77ebca58746d950
Alice <alice@Alices-Laptop.local>
2025-02-27 11:12:40 +0100

    EP2 - Utilisation des Enums

---
+++ s5/ep2/src/app.ts
@@ -0,0 +1,5 @@
+import { Direction, vectorDirection } from "./enums";
+console.log(`Direction North: ${vectorDirection(Direction.North)}`);
+console.log(`Direction East: ${vectorDirection(Direction.East)}`);
+console.log(`Direction South: ${vectorDirection(Direction.South)}`);
+console.log(`Direction West: ${vectorDirection(Direction.West)}`);
